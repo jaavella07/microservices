ce6af78ab0838fb222eb832146cc691970b9f082
Jorge Avella <79177900+jaavella07@users.noreply.github.com>
2026-02-17 22:55:13 -0500

    update MS-Payments

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,8 +1,27 @@
+import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
-import { PaymentsModule } from './payments.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(PaymentsModule);
-  await app.listen(process.env.port ?? 3000);
+
+  const logger = new Logger('Payments-ms');
+
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe
+    ({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+
+  logger.log(`Payments Microservice running on port ${envs.port}`);
 }
 bootstrap();
--- apps/payments/src/payments/payments.module.ts
+++ apps/payments/src/payments/payments.module.ts
@@ -3,7 +3,7 @@ import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
 @Module({
-  imports: [],
+
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
